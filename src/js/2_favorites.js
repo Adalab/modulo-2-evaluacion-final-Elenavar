e50d7ece3a96075185a56c7ef6cbd82efc12bdbf
Elena <elenavargasm9@gmail.com>
2022-11-15 11:04:50 +0100

    add delete favorites

--- src/js/2_favorites.js
+++ src/js/2_favorites.js
@@ -6,15 +6,22 @@ function findFavoritesCharacters(event) {
   const favoritesIndex = favoritesCharacters.findIndex(
     (char) => char.char_id === parseInt(event.currentTarget.id)
   );
+  console.log(favoritesIndex);
   if (favoritesIndex === -1) {
     favoritesCharacters.push(selectedCharacter);
     renderFavoriteCharacter(selectedCharacter);
   }
 }
 
+function addFavoritesEvents() {
+  const cross = document.querySelector('.cross');
+  cross.addEventListener('click', handleDelete);
+}
+
 function renderFavoriteCharacter(selectedCharacter) {
   const icon = document.createElement('p');
   icon.classList.add('cross');
+  icon.setAttribute('id', selectedCharacter.char_id);
   const x = document.createTextNode('x');
   icon.appendChild(x);
   favoritesList.appendChild(icon);
@@ -26,6 +33,23 @@ function renderFavoriteCharacter(selectedCharacter) {
       selectedCharacter.char_id
     )
   );
+  addFavoritesEvents();
+}
+
+function handleDelete(event) {
+  console.log(event.currentTarget);
+  localStorage.removeItem('favorites');
+  const favoritesFiltered = favoritesCharacters.filter(
+    (char) => char.char_id !== parseInt(event.currentTarget.id)
+  );
+  console.log(event.currentTarget.id);
+  console.log(favoritesFiltered);
+  favoritesList.innerHTML = '';
+  for (const fav of favoritesFiltered) {
+    renderFavoriteCharacter(fav);
+  }
+  localStorage.setItem('favorites', JSON.stringify(favoritesFiltered));
+  favoritesCharacters = favoritesFiltered;
 }
 
 fetch('https://www.breakingbadapi.com/api/characters')
@@ -35,7 +59,11 @@ fetch('https://www.breakingbadapi.com/api/characters')
     renderAllCharacters(allCharacters);
   });
 
-favoritesCharacters = JSON.parse(localStorage.getItem('favorites'));
-for (const favorite of favoritesCharacters) {
-  renderFavoriteCharacter(favorite);
+const favoritesSaved = JSON.parse(localStorage.getItem('favorites'));
+
+if (favoritesSaved !== null) {
+  favoritesCharacters = favoritesSaved;
+  for (const favorite of favoritesCharacters) {
+    renderFavoriteCharacter(favorite);
+  }
 }
